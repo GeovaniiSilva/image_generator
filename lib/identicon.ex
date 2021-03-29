75d9e52cb820c99a4528800ba438d4345c643a87
Adolfo Neto <adolfo.usp@gmail.com>
2021-03-29 20:08:49 -0300

    Added some tests

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -15,6 +15,29 @@ defmodule Identicon do
     |> save_image(input)
   end
 
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [red, green, blue | _tail]} = image) do
+    %Identicon.Image{image | color: {red, green, blue}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
   def save_image(image, filename) do
     File.write("#{filename}.png", image)
   end
@@ -60,28 +83,5 @@ defmodule Identicon do
 
   def mirror_row(row) do
     row
-  end
-
-  def build_grid(%Identicon.Image{hex: hex} = image) do
-    grid =
-      hex
-      |> Enum.chunk_every(3)
-      |> Enum.map(&mirror_row/1)
-      |> List.flatten()
-      |> Enum.with_index()
-
-    %Identicon.Image{image | grid: grid}
-  end
-
-  def pick_color(%Identicon.Image{hex: [red, green, blue | _tail]} = image) do
-    %Identicon.Image{image | color: {red, green, blue}}
-  end
-
-  def hash_input(input) do
-    hex =
-      :crypto.hash(:md5, input)
-      |> :binary.bin_to_list()
-
-    %Identicon.Image{hex: hex}
   end
 end
--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,6 +2,86 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
+  import Identicon
+
+  test "Hash input" do
+    teste_hex = [
+      105,
+      141,
+      193,
+      157,
+      72,
+      156,
+      78,
+      77,
+      183,
+      62,
+      40,
+      167,
+      19,
+      234,
+      176,
+      123
+    ]
+
+    teste_grid = [
+      {105, 0},
+      {141, 1},
+      {193, 2},
+      {141, 3},
+      {105, 4},
+      {157, 5},
+      {72, 6},
+      {156, 7},
+      {72, 8},
+      {157, 9},
+      {78, 10},
+      {77, 11},
+      {183, 12},
+      {77, 13},
+      {78, 14},
+      {62, 15},
+      {40, 16},
+      {167, 17},
+      {40, 18},
+      {62, 19},
+      {19, 20},
+      {234, 21},
+      {176, 22},
+      {234, 23},
+      {19, 24},
+      {123, 25}
+    ]
+
+    "teste"
+    |> hash_input
+    |> assert_hex(teste_hex)
+    |> pick_color
+    |> assert_color({105, 141, 193})
+    |> build_grid
+    |> assert_grid(teste_grid)
+
+    # |> filter_odd_squares
+    # |> build_pixel_map
+    # |> draw_image
+    # |> save_image(input)
+  end
+
+  defp assert_hex(image, hex) do
+    assert image.hex == hex
+    image
+  end
+
+  defp assert_color(image, color) do
+    assert image.color == color
+    image
+  end
+
+  defp assert_grid(image, grid) do
+    assert image.grid == grid
+    image
+  end
+
   test "identicon generate hash list" do
     image = Identicon.hash_input("test")
 
